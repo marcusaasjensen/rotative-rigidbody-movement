11b04a5ea378e6d1e87ac3978c5764301db044f8
Marcus Aas Jensen <88457743+marcusaasjensen@users.noreply.github.com>
2021-08-11 02:21:29 +0200

    Add files via upload

---
+++ DirectionVector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionVector : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float length;
+    public float size;
+
+    private float directionIntensity;
+    private float offSet;
+    private float magnitude;
+    void Update()
+    {
+        directionIntensity = Mathf.Abs(rb.velocity.x + rb.velocity.z);
+        offSet = transform.parent.transform.localScale.x/2;
+        magnitude = directionIntensity * length / 2;
+
+        transform.localScale = new Vector3(magnitude * Input.GetAxisRaw("Vertical") + size, size, magnitude * -Input.GetAxisRaw("Horizontal") + size);
+        transform.localPosition = new Vector3((magnitude + offSet) * Input.GetAxisRaw("Vertical"), transform.localPosition.y, (magnitude + offSet) * -Input.GetAxisRaw("Horizontal"));
+    }
+}
