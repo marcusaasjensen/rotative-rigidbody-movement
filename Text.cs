eb7f25138a33b85f2f662a2e9de289dde4812229
Marcus Aas Jensen <88457743+marcusaasjensen@users.noreply.github.com>
2021-08-11 02:41:43 +0200

    Add files via upload

---
+++ Text.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Text : MonoBehaviour
+{
+    public Rigidbody player;
+    public TextMeshProUGUI text;
+
+    void Update()
+    {
+        text.text = "velocity.x = " + (Mathf.Round(player.velocity.x*100)/100).ToString() + ", velocity.z = " + (Mathf.Round(player.velocity.z*100)/100).ToString() + ", Cos(rot) = " + (Mathf.Round(Mathf.Cos(player.gameObject.transform.eulerAngles.y*Mathf.Deg2Rad)*100)/100).ToString() + ", Sin(rot) = " + (Mathf.Round(Mathf.Sin(player.gameObject.transform.eulerAngles.y*Mathf.Deg2Rad)*100)/100).ToString() + ", rot° = " + (Mathf.Round(player.gameObject.transform.eulerAngles.y*100)/100).ToString();
+    }
+}
